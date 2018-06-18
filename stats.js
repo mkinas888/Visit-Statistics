c7218355681fd5d1cb11bb9108e35a2bbd6eebac
mkinas888 <mkinas888@gmail.com>
2018-06-18 14:52:04 +0200

    Basic average done

--- stats.js
+++ stats.js
@@ -1,3 +1,39 @@
-const getAverage = () => {};
+const getAverage = (series, isEachWeekDayAverage = false) => {
+    let finalAverage = {};
+    let entriesNumber = 0;
+    if(isEachWeekDayAverage) {
+        finalAverage = {
+            Monday: {
+                averageVisits: 0
+            },
+            Tuesday: {
+                averageVisits: 0
+            },
+            Wednesday: {
+                averageVisits: 0
+            },
+            Thursday: {
+                averageVisits: 0
+            },
+            Friday: {
+                averageVisits: 0
+            },
+            Saturday: {
+                averageVisits: 0
+            },
+            Sunday: {
+                averageVisits: 0
+            }
+        }
+    } else {
+        finalAverage = {averageVisits: 0};
+        series.forEach( elem => {
+            ++entriesNumber;
+            finalAverage.averageVisits += elem.visits;
+        });
+        finalAverage.averageVisits /= entriesNumber;
+    }
+    return finalAverage;
+};
 
 module.exports = getAverage;
