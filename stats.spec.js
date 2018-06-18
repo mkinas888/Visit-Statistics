0599b9e3dcbac191bfd2544468ee687965d729b5
mkinas888 <mkinas888@gmail.com>
2018-06-18 18:01:18 +0200

    Series tests added

--- stats.spec.js
+++ stats.spec.js
@@ -43,3 +43,59 @@ describe('getAverage', () => {
     });
   });
 });
+
+describe('getAverage', () => {
+  let firstWeek = [
+    { date: new Date('2018-01-01'), visits: 32 },
+    { date: new Date('2018-01-02'), visits: 82 },
+    { date: new Date('2018-01-03'), visits: 74 },
+    { date: new Date('2018-01-04'), visits: 38 },
+    { date: new Date('2018-01-05'), visits: 54 },
+    { date: new Date('2018-01-06'), visits: 64 },
+    { date: new Date('2018-01-07'), visits: 44 },
+  ];
+  let secondWeek = [
+    { date: new Date('2018-01-13'), visits: 24 },
+    { date: new Date('2018-01-14'), visits: 56 },
+    { date: new Date('2018-01-12'), visits: 94 },
+    { date: new Date('2018-01-11'), visits: 32 },
+    { date: new Date('2018-01-10'), visits: 68 },
+    { date: new Date('2018-01-09'), visits: 84 },
+    { date: new Date('2018-01-08'), visits: 52 },
+  ];
+
+  let series = firstWeek.concat(secondWeek);
+
+  test('average of series', () => {
+    expect(getAverage(series)).toEqual({
+      averageVisits: 57,
+    });
+  });
+
+  test('week days average of series', () => {
+    expect(getAverage(series, true)).toEqual({
+      Monday: {
+        averageVisits: 42,
+      },
+      Tuesday: {
+        averageVisits: 83,
+      },
+      Wednesday: {
+        averageVisits: 71,
+      },
+      Thursday: {
+        averageVisits: 35,
+      },
+      Friday: {
+        averageVisits: 74,
+      },
+      Saturday: {
+        averageVisits: 44,
+      },
+      Sunday: {
+        averageVisits: 50,
+      },
+    });
+  });
+});
+
