25ff03e6dab92de48fc692bb13890e4cf532af29
mkinas888 <mkinas888@gmail.com>
2018-06-19 05:17:38 +0200

    Make solution more concise

--- stats.js
+++ stats.js
@@ -1,95 +1,60 @@
 const getAverage = (series, isEachWeekDayAverage = false) => {
     let finalAverage = {};
     let entriesNumber = 0;
-    let mondayEntries = 0, tuesdayEntries = 0, wednesdayEntries = 0, thursdayEntries = 0, 
-        fridayEntries = 0, saturdayEntries = 0, sundayEntries = 0;
+    const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     if(isEachWeekDayAverage) {
+        // if none entries in series the average is zero
         finalAverage = {
-            "Monday": {
+            'Monday': {
                 averageVisits: 0
             },
-            "Tuesday": {
+            'Tuesday': {
                 averageVisits: 0
             },
-            "Wednesday": {
+            'Wednesday': {
                 averageVisits: 0
             },
-            "Thursday": {
+            'Thursday': {
                 averageVisits: 0
             },
-            "Friday": {
+            'Friday': {
                 averageVisits: 0
             },
-            "Saturday": {
+            'Saturday': {
                 averageVisits: 0
             },
-            "Sunday": {
+            'Sunday': {
                 averageVisits: 0
             }
         }
-        series.forEach( elem =>{
-            let whichDay = elem.date.getDay();
-            switch(whichDay) {
-                case 0:
-                    ++sundayEntries;
-                    finalAverage.Sunday.averageVisits += elem.visits;
-                    break;
-                case 1:
-                    ++mondayEntries;
-                    finalAverage.Monday.averageVisits += elem.visits;
-                    break;
-                case 2:
-                    ++tuesdayEntries;
-                    finalAverage.Tuesday.averageVisits += elem.visits;
-                    break;
-                case 3:
-                    ++wednesdayEntries;
-                    finalAverage.Wednesday.averageVisits += elem.visits;
-                    break;
-                case 4:
-                    ++thursdayEntries;
-                    finalAverage.Thursday.averageVisits += elem.visits;
-                    break;
-                case 5:
-                    ++fridayEntries;
-                    finalAverage.Friday.averageVisits += elem.visits;
-                    break;
-                case 6:
-                    ++saturdayEntries;
-                    finalAverage.Saturday.averageVisits += elem.visits;
-                    break;
+        // check each week day
+        for(let i = 0; i < 7; i++) {
+            entriesNumber = 0;
+            for(let j = 0; j < series.length; j++) {
+                // execute if week day is correct
+                if(series[j].date.getDay() === i) {
+                    ++entriesNumber;
+                    finalAverage[weekDays[i]].averageVisits += series[j].visits;
+                }
             }
-        });
-        if(sundayEntries !== 0) {
-            finalAverage.Sunday.averageVisits /= sundayEntries;
-        }
-        if(mondayEntries !== 0) {
-            finalAverage.Monday.averageVisits /= mondayEntries;
-        }
-        if(tuesdayEntries !== 0) {
-            finalAverage.Tuesday.averageVisits /= tuesdayEntries;
-        }
-        if(wednesdayEntries !== 0) {
-            finalAverage.Wednesday.averageVisits /= wednesdayEntries;
-        }
-        if(thursdayEntries !== 0) {
-            finalAverage.Thursday.averageVisits /= thursdayEntries;
-        }
-        if(fridayEntries !== 0) {
-            finalAverage.Friday.averageVisits /= fridayEntries;
-        }
-        if(saturdayEntries !== 0) {
-            finalAverage.Saturday.averageVisits /= saturdayEntries;
+            // if any entries that day 
+            if(entriesNumber !== 0) {
+                finalAverage[weekDays[i]].averageVisits /= entriesNumber;
+            }
         }
     } else {
         finalAverage = {
             averageVisits: 0
         };
+        // loop through all entries
         series.forEach( elem => {
             ++entriesNumber;
             finalAverage.averageVisits += elem.visits;
         });
-        finalAverage.averageVisits /= entriesNumber;
+        // if series isn't empty
+        if(entriesNumber !== 0) {
+            finalAverage.averageVisits /= entriesNumber;
+        }
     }
     return finalAverage;
 };
